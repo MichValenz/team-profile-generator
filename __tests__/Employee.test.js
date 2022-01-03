65633667d9c0ad1c79cf780c4ef53b9e3dfeec63
MichValenz <michvalenz27@gmail.com>
2022-01-02 21:14:29 -0600

    added test for employee details

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,3 +1,13 @@
-// function Employee(name){
-//     this.name = name
-// }
+const Employee = require("../lib/Employee");
+
+jest.mock("../lib/Employee");
+
+console.log(new Employee())
+
+test("creates an employee object", () => {
+    const employee = new Employee(String);
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+    expect(employee.officeNumber).toEqual(expect.any(Number));
+});
